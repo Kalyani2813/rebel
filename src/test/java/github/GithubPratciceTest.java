b41c326e88eaa0794b9b334ec5d3ec6eb16d4fc9
Kalyani <Kalyani@Kalyanihp>
2025-12-03 15:16:42 +0530

    First commit

---
+++ src/test/java/github/GithubPratciceTest.java
@@ -0,0 +1,13 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPratciceTest{
+	@Test
+	public void githubPracticeTest() {
+		Reporter.log("Hii github",true);
+//		String BROWSER = System.getProperty("browser");
+//		System.out.println(BROWSER);
+	}
+}
---
+++ src/test/java/maven/MavenPracticeTest.java
@@ -0,0 +1,10 @@
+package maven;
+
+import org.testng.annotations.Test;
+
+public class MavenPracticeTest {
+	@Test
+	public void mavenPracticeTest() {
+		System.out.println("Hii Maven");
+	}
+}
